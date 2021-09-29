400cd0b12ad2c38009073b28981be172aab8ff33
AnOpportunist <svyatoslavkritsky@gmail.com>
2021-09-29 21:40:43 +0300

    Добавил файл sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x, y;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    std::cout << std::endl << "Введите y: ";
+    std::cin >> y;
+    std::cout << x + y << std::endl;
+    return 0;
+}
