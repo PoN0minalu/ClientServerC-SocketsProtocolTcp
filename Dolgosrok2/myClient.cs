f61bb2f844a78894e2731100bb98afa75a40dc53
PoN0minalu <91563831+PoN0minalu@users.noreply.github.com>
2021-10-26 22:00:38 +0300

    Обновил клиент
    Добавил обратную связь с сервером

--- Dolgosrok2/myClient.cs
+++ Dolgosrok2/myClient.cs
@@ -1,10 +1,14 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using myFirstProtocol;
+using myInterface;
+using System.Windows.Input;
+using System.Text;
 
-namespace Dolgosrok2
+namespace myClient
 {
+
     class Program
     {
         static int port = 8005;
@@ -14,37 +18,53 @@ namespace Dolgosrok2
             try
             {
                 IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
- 
+
                 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 // подключаемся к удаленному хосту
                 socket.Connect(ipPoint);
-                //==========================================================
-                TMPD1Packet sendPacket = new TMPD1Packet(1);                
-                sendPacket.SetPathToFile("/home/svyatoslaw/tests/testProgram/bin/Debug/net5.0/testProgram");
-                sendPacket.SetParamsOfExe("-h");
-                socket.Send(sendPacket.ToPack());
-                //==========================================================
-                // получаем ответ
-                byte[] data = new byte[256]; // буфер для ответа
-                
-                int bytes = 0; // количество полученных байт
- 
-                do
+                while (true)
                 {
-                    bytes = socket.Receive(data);
+
+                    Interface face = new Interface();
+                    int toDo = face.GetDo();
+                    TMPD1Packet sendPacket = new TMPD1Packet(toDo);
+                    switch(toDo)
+                    {
+                        case 1:
+                            sendPacket.SetPathToFile(face.GetResult());
+                            sendPacket.SetPathToFile(face.GetParam());
+                            break;
+                        case 2:
+                            sendPacket.SetPathToFile(face.GetResult());
+                            break;
+                    }
+                    if (toDo == 3 || toDo == 4)
+                    {
+                        sendPacket.SetPathToFile(face.GetResult());
+                        sendPacket.SetPathToFile(face.GetFinal());
+                    }
+                    sendPacket.SetPathToFile(face.GetResult());
+                    socket.Send(sendPacket.ToPack());
+                    // получаем ответ
+                    byte[] data = new byte[256]; // буфер для ответа
+                    int bytes = 0; // количество полученных байт
+                    do
+                    {
+                        bytes = socket.Receive(data);
+                    }
+                    while (socket.Available > 0);
+                    TMPD1Packet getPacket = new TMPD1Packet(0);
+                    getPacket = TMPD1Packet.ToParse(data);
+                    ManagerOfPackets boss = new ManagerOfPackets(getPacket);
+                    boss.DirtyWork();
+                    Console.WriteLine("\nBackspace - menu\nESC - exit");
+                    face.CheckBackESC();
                 }
-                while (socket.Available > 0);
-
-                TMPD1Packet getPacket = new TMPD1Packet(0);
-                getPacket = TMPD1Packet.ToParse(data);
-                ManagerOfPackets boss = new ManagerOfPackets(getPacket);
-                boss.DirtyWork();
-
                 // закрываем сокет
                 socket.Shutdown(SocketShutdown.Both);
                 socket.Close();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
